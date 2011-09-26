123f4c6d942cddc0a5894dbfd1566ade8de120a7
Zhuan Qingshan <zhuanqingshan@163.com>
2011-09-26 22:38:54 +0800

    add StoreController and 3 methods(Index,Browse,Details)

---
+++ MvcMusicStore-StepByStep/Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore_StepByStep.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Hello from store.Index()";
+        }
+
+        public string Browse()
+        {
+            return "Hello from store.Browse()";
+        }
+
+        public string Details()
+        {
+            return "Hello from store.Details()";
+        }
+
+    }
+}
